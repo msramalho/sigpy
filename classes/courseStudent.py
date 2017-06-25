af9f28e4152ecc40ddbbd08f272494f8c9d81e27
msramalho <up201403027@fe.up.pt>
2017-06-25 12:40:18 +0100

    improved courseStudent

--- classes/courseStudent.py
+++ classes/courseStudent.py
@@ -1,10 +1,9 @@
 import sys
 sys.path.append('../')
 from classes.course import *
-from utils import constructString
 class courseStudent(course):
-    def __init__(self, initials="", id="", cod="", name="", degree="", type="", start="", duration="", subjectsLink="", year="", state="", enrolled="", institution = "", director="", directorAdj=""):
-        super(courseStudent, self).__init__(initials, id, cod, name, degree, type, start, duration, subjectsLink, director, directorAdj)
+    def __init__(self, initials="", id="", cod="", name="", degree="", type="", start="", duration="", subjectsLink="", year="", state="", enrolled="", institution = "", director="", directorAdj="", subjects = []):
+        super(courseStudent, self).__init__(initials, id, cod, name, degree, type, start, duration, subjectsLink, director, directorAdj, subjects)
         self.institution=institution
         self.year=year
         self.enrolled=enrolled
