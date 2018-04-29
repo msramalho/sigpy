721c131da4f6e40634caf752910c466a864efc3a
msramalho <up201403027@fe.up.pt>
2018-04-29 01:52:38 +0100

    updated main

--- main.py
+++ main.py
@@ -1,26 +1,19 @@
-from faculties.selector import getFaculty
+from faculties.selector import get_faculty
 from faculties.interface import interface
 
-fac = getFaculty("feup")
 
 #fac.findSubjects("https://sigarra.up.pt/feup/pt/cur_geral.cur_planos_estudos_view?pv_plano_id=2496&pv_ano_lectivo=2016&pv_tipo_cur_sigla=&pv_origem=CUR#div_id_285926")
 
-i = interface("test")
-i.startSession("up201403027")
-exit()
-#Test session start
-print("Login: %s" % fac.startSession("up201403027"))
+# load FEUP
+fac = get_faculty("feup")
 
+# Login into sigarra
+p = "TODO"
+print("Login: %s" % fac.login("up201403027", p))
 
-#Test find student
+# get a student
+me = fac.get_student("201403027")
+print(me)
 
-#fac.setLoad("loadCourses", True)
-fac.setLoad("loadPictures", True)
-mig = fac.findStudent("201403027")
-mig.picture.show()
-
-c = input("")
-#Test find
-
-
-
+# get a student picture and display it
+# fac.get_picture(me.id).show()
