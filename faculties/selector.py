7f532e5ecd83d933c4defb908a35639852c66dac
msramalho <up201403027@fe.up.pt>
2018-04-29 01:27:14 +0100

    fixed selector for snake_case

--- faculties/selector.py
+++ faculties/selector.py
@@ -1,10 +1,12 @@
-import sys, os.path, importlib
+import sys
+import os.path
+import importlib
 sys.path.append('../')
 
 
-def getFaculty(faculty = "feup"):
-    if not os.path.isfile("faculties/%s.py" % faculty):#not supported
+def get_faculty(faculty="feup"):
+    if not os.path.isfile("faculties/%s.py" % faculty):  # not supported
         print("The faculty %s has no file to import" % faculty)
         exit(1)
-    mod = importlib.import_module("faculties.%s"%faculty)#import the correct file
+    mod = importlib.import_module("faculties.%s" % faculty)  # import the correct file
     return mod.faculty()
