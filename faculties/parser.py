e077a80773f88c8441e9561e7a0d91954d6fb8de
msramalho <up201403027@fe.up.pt>
2018-05-06 01:29:33 +0100

    error testing + derivate

--- faculties/parser.py
+++ faculties/parser.py
@@ -41,23 +41,36 @@ def parse_xpath(tree, config):
         el = el.text_content()
     return el.strip()
 
+# given a config dict with string "derivate" and tuple "from", match both and return
+def parse_derivate(config, res):
+    return config["derivate"] % tuple(res[t_el] for t_el in config["from"])
 
 # given an lxml tree and a config dict with one of [css, regex, xpath] key, get its VALUE
-def parse_attribute(tree, config):
-    if "css" in config:  # this is an attribute from css
-        return parse_css(tree, config)
-    elif "regex" in config:  # this is an attribute from regex
-        return parse_regex(tree, config)
-    elif "xpath" in config:
-        return parse_xpath(tree, config)
+def parse_attribute(tree, config, res):
+    try:
+        if "css" in config:  # this is an attribute from css
+            return parse_css(tree, config)
+        elif "regex" in config:  # this is an attribute from regex
+            return parse_regex(tree, config)
+        elif "xpath" in config:  # this is an attirbute from xpath
+            return parse_xpath(tree, config)
+        elif "derivate" in config: # this is an attribute derivated from another
+            return parse_derivate(config, res)
+    except Exception as e: # some attributes do not exist if not logged in
+        print("Error: %s when parsing %s... Ignoring attribute" % (str(e), config))
+        return None
 
 
 # given an lxml tree and a config dict with one of [css, xpath] key, get its lxml ELEMENT
 def parse_element(tree, config):
-    if "css" in config:  # this is an attribute from css
-        return tree.cssselect(config["css"])
-    elif "xpath" in config:
-        return tree.xpath(config["xpath"])[get_index(config)]
+    try:
+        if "css" in config:  # this is an attribute from css
+            return tree.cssselect(config["css"])
+        elif "xpath" in config:
+            return tree.xpath(config["xpath"])[get_index(config)]
+    except Exception as e: # some attributes do not exist if not logged in
+        print("Error: %s when parsing %s... Ignoring element" % (str(e), config))
+        return []
 
 
 # given an lxml element and a config, parse a class's attributes and create a new class from them
@@ -76,7 +89,7 @@ def parse_attributes(tree, attributes, original=None):
             continue
         # else load from the configurations
         if "model" not in config:  # this is a simple attr with direct css
-            res[attr] = parse_attribute(tree, config)
+            res[attr] = parse_attribute(tree, config, res)
         elif "model" in config:  # handle classes
             element = parse_element(tree, config)
             if "list" in config:  # handle list of said class
