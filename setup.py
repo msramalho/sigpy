9827a72f874d24cc714c821f5d1a347bf6916f7d
msramalho <up201403027@fe.up.pt>
2018-09-14 19:33:50 +0100

    setup file

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup
+
+setup(name='sigpy',
+      version='1.0',
+      description='Sigarra Python API based on Recursive Web Scraping Parser (wtf)',
+      url='https://github.com/msramalho/sigpy',
+      author='msramalho',
+      license='MIT',
+      packages=['sigpy'],
+      zip_safe=False)
