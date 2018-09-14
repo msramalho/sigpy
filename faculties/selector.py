8590bb94ba978a3c380e945e5ae584627a7edfb8
msramalho <up201403027@fe.up.pt>
2018-09-14 19:29:27 +0100

    not so abrupt abort

--- faculties/selector.py
+++ faculties/selector.py
@@ -6,7 +6,6 @@ sys.path.append('../')
 
 def get_faculty(faculty="feup"):
     if not os.path.isfile("faculties/%s.py" % faculty):  # not supported
-        print("The faculty %s has no file to import" % faculty)
-        exit(1)
+        raise Exception("The faculty %s has no file to import" % faculty)
     mod = importlib.import_module("faculties.%s" % faculty)  # import the correct file
     return mod.faculty()
