462d38e9dd41cb499ce5c40be34f309aa4b380bf
msramalho <up201403027@fe.up.pt>
2018-09-16 11:50:46 +0100

    new feup routes

--- sigpy/faculties/feup/__init__.py
+++ sigpy/faculties/feup/__init__.py
@@ -9,6 +9,8 @@ class faculty(interface):
     interface.routes["teacher"] = "https://sigarra.up.pt/feup/pt/func_geral.formview?p_codigo=%s"
     interface.routes["teacher_schedule"] = "https://sigarra.up.pt/feup/pt/hor_geral.docentes_view?pv_doc_codigo=%s"
     interface.routes["course"] = "https://sigarra.up.pt/feup/pt/cur_geral.cur_view?pv_curso_id=%s&pv_ano_lectivo=%d" % ("%s", getSchoolYear())
+    interface.routes["study_plan"] = "https://sigarra.up.pt/feup/pt/cur_geral.cur_planos_estudos_view?pv_plano_id=%s&pv_ano_lectivo=%s&pv_tipo_cur_sigla=&pv_origem=CUR"
+    interface.routes["subject"] = "https://sigarra.up.pt/feup/pt/ucurr_geral.ficha_uc_view?pv_ocorrencia_id=%s"
     interface.routes["picture"] = "https://sigarra.up.pt/feup/pt/fotografias_service.foto?pct_cod=%s"
     interface.routes["room"] = "https://sigarra.up.pt/feup/pt/instal_geral.espaco_view?pv_id=%s"
     interface.routes["room_picture"] = "https://sigarra.up.pt/feup/pt/instal_geral2.get_mapa?pv_id=%s"
